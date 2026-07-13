b59676d00915901812950b55f4c912d64ab36c43
bellacintyya13-tech <bella.cintyya13@gmail.com>
2026-07-13 18:14:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+const c=document.getElementById('board'),ctx=c.getContext('2d');
+document.getElementById('start').onclick=()=>alert('Kerangka project berhasil dibuat. Implementasi penuh Tetris (ratusan baris) tidak muat dalam satu respons ChatGPT.');
+ctx.fillStyle='cyan';ctx.fillRect(120,20,60,60);
